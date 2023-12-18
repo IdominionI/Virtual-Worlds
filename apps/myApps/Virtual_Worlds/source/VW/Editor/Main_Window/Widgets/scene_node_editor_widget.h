fff8a1d554f3224c1601933e03586a16bf1fed01
dominion <ddomino@internode.on.net>
2023-12-18 15:39:59 +1030

    test change
    test change

--- apps/myApps/Virtual_Worlds/source/VW/Editor/Main_Window/Widgets/scene_node_editor_widget.h
+++ apps/myApps/Virtual_Worlds/source/VW/Editor/Main_Window/Widgets/scene_node_editor_widget.h
@@ -19,10 +19,6 @@ inline node_id_type current_active_editor = INVALID_ID;
 // procesess as part of the nodes funtion.
 //inline scene_graph_class master_graph;
 
-
-
-
-
 class scene_node_editor_class : public node_editor_basis_class {
 public:
     bool         display_on = true;
